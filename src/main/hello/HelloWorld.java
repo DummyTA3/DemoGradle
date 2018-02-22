c33443b0f36b6bc26bfa52c0d478b25848d23e0a
Akshay Narayan <okkhoy@gmail.com>
2018-02-22 23:47:47 +0800

    Create main method and say hello! Fixes #4

--- src/main/hello/HelloWorld.java
+++ src/main/hello/HelloWorld.java
@@ -1,4 +1,9 @@
 package main.hello;
 
 public class HelloWorld {
+
+    public static void main(String [] args) {
+        Greetings g = new Greetings();
+        System.out.println(g.sayHello("cs2102 class"));
+    }
 }
