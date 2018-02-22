f7d2be6437061843e1bc886c75237f098f597380
Akshay Narayan <okkhoy@gmail.com>
2018-02-23 00:15:04 +0800

    Add time from joda time.

--- src/main/hello/HelloWorld.java
+++ src/main/hello/HelloWorld.java
@@ -1,9 +1,16 @@
 package main.hello;
 
+import org.joda.time.LocalTime;
+
 public class HelloWorld {
 
     public static void main(String [] args) {
+
         Greetings g = new Greetings();
         System.out.println(g.sayHello("cs2102 class"));
+
+        LocalTime timeNow = new LocalTime();
+        System.out.println("Time now is: " + timeNow);
+        
     }
 }
