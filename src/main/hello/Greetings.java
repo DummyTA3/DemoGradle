1ce70def9c9b8004e1f0ea50a208e41ee9a33d1d
Akshay Narayan <okkhoy@gmail.com>
2018-02-22 23:26:18 +0800

    add sayHello method for basic greeting

--- src/main/hello/Greetings.java
+++ src/main/hello/Greetings.java
@@ -4,4 +4,13 @@ package main.hello;
  * Contains various greeting (hello!) messages
  */
 public class Greetings {
+
+    /**
+     * Says hello to the person passed as argument
+     * @param name of the person
+     * @return Hello {Name}
+     */
+    public String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
 }
