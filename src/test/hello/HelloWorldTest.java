d9583e553c490acc889d8260beccc4a1afbb59a2
Akshay Narayan <okkhoy@gmail.com>
2018-02-23 12:44:18 +0800

    Organize imports for test driver

--- src/test/hello/HelloWorldTest.java
+++ src/test/hello/HelloWorldTest.java
@@ -1,5 +1,9 @@
 package test.hello;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 public class HelloWorldTest {
 
     public static void main(String [] args) {
